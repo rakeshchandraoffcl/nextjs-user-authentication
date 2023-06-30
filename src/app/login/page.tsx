c39c291570f788ccbff7a27ea961414c2ae0842c
Rakesh Chandra Dash <rakeshchandradash.dev@gmail.com>
2023-06-30 23:44:08 +0530

    Basic file structure

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+	return (
+		<div>
+			<p className="text-center text-lg">Login Page</p>
+		</div>
+	);
+};
+
+export default Login;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Signup = () => {
+	return (
+		<div>
+			<p className="text-center text-lg">Signup Page</p>
+		</div>
+	);
+};
+
+export default Signup;
